015852451700f1423246f4da31cfe0fb1c885293
Voronovich_dev <alexanderbizv1@gmail.com>
2022-09-02 23:35:36 +0300

    added error handling layout

--- script.js
+++ script.js
@@ -1,77 +1,60 @@
 // general response
 async function getWeather(city) {
-   try {
-      const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=db968690a857f3cea0bf82c3cf38f5ad`)
-      return await response.json()
-   } catch (err) {
-      console.log(err)
-   }
+
+   const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=db968690a857f3cea0bf82c3cf38f5ad`)
+   return await response.json()
+
 }
 
 
 // temperature
 async function getTemp(city) {
-   try {
-      const cityInfo = await getWeather(city)
-      const tempObj = {
-         temperature: Math.round(cityInfo.main.temp),
-         feels_like: Math.round(cityInfo.main.feels_like),
-         temp_max: Math.round(cityInfo.main.temp_max),
-         temp_min: Math.round(cityInfo.main.temp_min),
-      }
-      return tempObj
+
+   const cityInfo = await getWeather(city)
+   const tempObj = {
+      temperature: Math.round(cityInfo.main.temp),
+      feels_like: Math.round(cityInfo.main.feels_like),
+      temp_max: Math.round(cityInfo.main.temp_max),
+      temp_min: Math.round(cityInfo.main.temp_min),
    }
-   catch (err) {
-      console.log(err)
-   }
+   return tempObj
+
 }
 
 //humidity
 async function getHumidity(city) {
-   try {
-      const cityInfo = await getWeather(city);
-      humidity = cityInfo.main.humidity;
-      return humidity
-   }
-   catch (err) {
-      console.log(err)
-   }
+
+   const cityInfo = await getWeather(city);
+   const humidity = cityInfo.main.humidity;
+   return humidity
+
 }
 
 //wind
 async function getWind(city) {
-   try {
-      const cityInfo = await getWeather(city);
-      const windSpeed = cityInfo.wind.speed;
-      return windSpeed
-   }
-   catch (err) {
-      console.log(err)
-   }
+
+   const cityInfo = await getWeather(city);
+   const windSpeed = cityInfo.wind.speed;
+   return windSpeed
+
 }
 
 //clouds
 async function getClouds(city) {
-   try {
-      const cityInfo = await getWeather(city);
-      clouds = cityInfo.weather[0].description;
-      return clouds
-   }
-   catch (err) {
-      console.log(err)
-   }
+
+   const cityInfo = await getWeather(city);
+   const clouds = cityInfo.weather[0].description;
+   return clouds
+
 }
 
 //cityName
 async function getCity(city) {
-   try {
-      const cityInfo = await getWeather(city)
-      const cityName = cityInfo.name
-      return cityName
-   }
-   catch (err) {
-      console.log(err)
-   }
+
+   const cityInfo = await getWeather(city)
+   const cityName = cityInfo.name
+   return cityName
+
 }
 
 // grouping all weather data
@@ -88,14 +71,12 @@ async function getAllWeather(city) {
          cityName, clouds, wind, humidity, tempObj
       }
       createApp(weatherObject)
-      // console.log(weatherObject)
 
    } catch (err) {
-      console.log(err)
+      createError()
    }
 }
 
-getAllWeather('paris')
 
 const searchForm = document.forms.search
 
@@ -107,6 +88,7 @@ searchForm.addEventListener('submit', async (e) => {
 })
 
 
+// fill our app with data from api
 function createApp(weatherObject) {
 
    const cityName = document.getElementById('city')
@@ -134,3 +116,34 @@ function createApp(weatherObject) {
    wind.textContent = `Wind: ${weatherObject.wind} m/s`
 
 }
+
+// error handling layout 
+function createError() {
+
+   const cityName = document.getElementById('city')
+   cityName.textContent = 'Check city name'
+
+   const currentTemp = document.querySelector('.current-temp p')
+   currentTemp.textContent = 'n/d'
+
+   const maxTemp = document.querySelector('.max-temp p')
+   maxTemp.textContent = 'n/d'
+
+   const minTemp = document.querySelector('.min-temp p')
+   minTemp.textContent = 'n/d'
+
+   const feelsLike = document.querySelector('.info-feel p')
+   feelsLike.textContent = 'n/d'
+
+   const clouds = document.querySelector('.info-clouds p')
+   clouds.textContent = 'n/d'
+
+   const humidity = document.querySelector('.info-humidity p')
+   humidity.textContent = 'n/d'
+
+   const wind = document.querySelector('.info-wind p')
+   wind.textContent = 'n/d'
+
+}
+// call function to initialize app 
+getAllWeather('paris')
